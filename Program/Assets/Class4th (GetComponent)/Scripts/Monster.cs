836cb9a307415d6074e162b0efc099b90d8f0ee3
chichi <yangju058@gmail.com>
2024-08-13 19:46:12 +0900

    (2024.8.13) 키 입력 시스템

---
+++ Program/Assets/Class4th (GetComponent)/Scripts/Monster.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Move))]
+
+public class Monster : MonoBehaviour
+{
+    [SerializeField] Move move;
+
+    private void Awake()
+    {
+        move = GetComponent<Move>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space Bar");
+        }
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            move.OnMove(Vector3.forward);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            move.OnMove(Vector3.left);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            move.OnMove(Vector3.back);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            move.OnMove(Vector3.right);
+        }
+
+    }
+}
